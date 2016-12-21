ae465f6992681c381aa7b2724dd202645d3bd0b4
Kai Stullich <kaistullich@rocketmail.com>
2016-12-21 10:32:28 -0800

    Able to input ticker name into URL to search

--- stockPrices.py
+++ stockPrices.py
@@ -1,8 +1,10 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
+userStockInput = input("Please enter stock ticker: ")
+
 # specify the url
-url = 'https://www.google.com/finance?cid=22144'
+url = ('https://finance.yahoo.com/quote/{}?ltr=1').format(userStockInput.upper())
 
 # query the website and return the html to the variable 'page'
 page = urlopen(url)
@@ -10,7 +12,7 @@ page = urlopen(url)
 # parse the html using beautiful soap and store in variable `soup`
 soup = BeautifulSoup(page, 'html.parser')
 
-# Finding AAPL id tag and only showing the text inside of the tag
-aapl = soup.find(id='ref_22144_l').text
+# Finding the ticker by class tag
+ticker = soup.find(class_='D(ib) Maw(65%) Maw(70%)--tab768 Ov(h)').text
 
-print('Current AAPL share price is:', aapl)
+print('Current {} share price is:'.format(userStockInput.upper()), ticker)
